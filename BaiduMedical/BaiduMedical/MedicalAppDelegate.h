34efe64ae93570f94bb5fcb4346a991d48f6bf17
yajun0601 <yajun0601@gmail.com>
2014-06-19 21:55:23 +0800

    create github project

---
+++ BaiduMedical/BaiduMedical/MedicalAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  MedicalAppDelegate.h
+//  BaiduMedical
+//
+//  Created by zhengyajun on 6/19/14.
+//  Copyright (c) 2014 zhengyajun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MedicalAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ BaiduMedical/BaiduMedical/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  BaiduMedical
+//
+//  Created by zhengyajun on 6/19/14.
+//  Copyright (c) 2014 zhengyajun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "MedicalAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([MedicalAppDelegate class]));
+    }
+}
